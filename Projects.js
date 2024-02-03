864eaf4d29dae7ad9e0c3e82f6b1bb5a61fb75d1
vngelforever <156696078+vngelforever@users.noreply.github.com>
2024-02-03 15:31:55 +0200

    Add files via upload

---
+++ Projects.js
@@ -0,0 +1,20 @@
+import VNGEL from './img/VNGEL.png'
+
+const Projects = [
+  { id:1,
+    title: 'VNGELHILL',
+    img : VNGEL
+  },
+
+  { id:2,
+    title: 'PRODUCTIZE',
+    img :'https://m.media-amazon.com/images/I/816NBImMMXS._AC_UY218_.jpg'
+  },
+
+  { id:3,
+    title: 'PIANO SOLOS',
+    img :'https://m.media-amazon.com/images/I/71lRLOtn4IL._AC_UY218_.jpg'
+  }
+]
+
+export {Projects};
---
+++ index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import {Index} from './App.js';
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Index />
+  </React.StrictMode>
+  , document.getElementById('root')
+);
+
